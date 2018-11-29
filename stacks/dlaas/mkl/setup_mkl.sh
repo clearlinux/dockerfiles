eb77a977b8d75830ebfefb199abfd521b47e6b2a
Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>
2018-11-29 10:53:39 +0000

    stacks/dlaas: dockerfiles for Clear Linux Deep Learning Stack
    This provides a Clear Linux Deep Learning Stack. To offer more flexibility,
    there are two versions of the Deep Learning Stack:  a "fully open source"
    version that includes only open source components, and a second, "mostly open
    source" version that includes the TensorFlow* framework optimized using Intel®
    Math Kernel Library for Deep Neural Networks (Intel® MKL-DNN) primitives.

---
+++ stacks/dlaas/mkl/setup_mkl.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+MINCONDA_VERSION="4.5.11"
+
+echo "Downloading miniconda..."
+curl -k -L --fail https://repo.anaconda.com/miniconda/Miniconda3-${MINCONDA_VERSION}-Linux-x86_64.sh \
+    -o ~/miniconda.sh 2>/dev/null || { echo "Failed to download MiniConda..."; }
+/bin/bash ~/miniconda.sh -b -p /opt/conda && rm ~/miniconda.sh
+mkdir -p /etc/profile.d && \
+    ln -s -f /opt/conda/etc/profile.d/conda.sh /etc/profile.d/conda.sh && \
+    echo ". /opt/conda/etc/profile.d/conda.sh" >> ~/.bashrc && \
+    source ~/.bashrc
+# create an env and install Tensorflow
+echo "Installing Tensorflow with MKL..."
+conda create -y  -n tf_env tensorflow nltk && \
+    echo "PATH=/opt/conda/envs/tf_env/bin:$PATH" >> ~/.bashrc && \
+    echo "conda activate tf_env" >> ~/.bashrc
+ln -s -f /opt/conda/envs/tf_env/bin/python /usr/bin/python
+ln -s -f /opt/conda/envs/tf_env/lib/python3.6/site-packages /opt/conda/lib/python3.7/site-packages
+source ~/.bashrc
