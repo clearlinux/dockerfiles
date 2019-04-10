5928d956813d05849f3f4ea2351c88c4d79c6dce
Qi Zheng <qi.zheng@intel.com>
2019-04-10 10:41:03 +0800

    Add elasticsearch docker image
    Signed-off-by: Qi Zheng <qi.zheng@intel.com>

---
+++ elasticsearch/docker-entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+export PATH=$PATH:/usr/share/elasticsearch/bin/
+
+# Add elasticsearch as command if needed
+if [ "${1:0:1}" = '-' ]; then
+	set -- elasticsearch "$@"
+fi
+
+# Drop root privileges if we are running elasticsearch
+# allow the container to be started with `--user`
+if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
+	# Change the ownership of user-mutable directories to elasticsearch
+	for path in \
+		/usr/share/elasticsearch/data \
+		/usr/share/elasticsearch/logs \
+	; do
+		chown -R elasticsearch:elasticsearch "$path"
+	done
+	
+	# use tmp file to avoid multiple parameters issue on su -c
+	echo "elasticsearch  -s /bin/bash -c \"$0 $@\"" | xargs su
+fi
+
+# As argument is not related to elasticsearch,
+# then assume that user wants to run his own process,
+# for example a `bash` shell to explore this image
+exec "$@"
