21323d1699a90331628b97913e9b94ec5264057c
Qi Zheng <qi.zheng@intel.com>
2019-07-09 06:30:26 +0000

    Disable redis protected mode
    The redis on docker hub disabled protected mode in default.
    To make clear redis be compatible, disable the protected mode
    in default as well.
    
    Signed-off-by: Qi Zheng <qi.zheng@intel.com>

--- redis/docker-entrypoint.sh
+++ redis/docker-entrypoint.sh
@@ -4,13 +4,12 @@ set -e
 # first arg is `-f` or `--some-option`
 # or first arg is `something.conf`
 if [ "${1#-}" != "$1" ] || [ "${1%.conf}" != "$1" ]; then
-	set -- redis-server "$@"
+    set -- redis-server "$@"
 fi
 
-# allow the container to be started with `--user`
+# change to redis user to run
 if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
-	find . \! -user redis -exec chown redis '{}' +
-	echo "su redis -s /bin/bash -c \"$0 $@\"" | exec
+    echo "redis -s /bin/bash -c \"$0 $@\"" | xargs su
 fi
 
 exec "$@"
