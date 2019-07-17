05981ccb0dd7ae152d07ad9cd7554b4eebd8e43d
Rusty Lynch <rusty.lynch@intel.com>
2019-07-17 14:14:34 -0700

    Fix startup crasher bug
    socket.gaierror errors out on startup since it is unable to resolve
    the NetbookApp.ip address.  This was reported upstream as:
    https://github.com/jupyter/notebook/issues/4558
    
    Signed-of-by: Rusty Lynch <rusty.lynch@intel.com>

--- machine-learning-ui/jupyter_notebook_config.py
+++ machine-learning-ui/jupyter_notebook_config.py
@@ -1,7 +1,7 @@
 c = get_config()
 c.ContentsManager.root_dir = "/root/"
 c.NotebookApp.allow_root = True
-c.NotebookApp.ip = '*'
+c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.open_browser = False
 c.NotebookApp.port = 8888
 
