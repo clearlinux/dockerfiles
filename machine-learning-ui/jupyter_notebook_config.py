3956d7c04c3905e1df98b61c9bee8c0c72a7cc9e
Liu, Jianjun <jianjun.liu@intel.com>
2019-08-26 05:35:05 +0000

    remove machine-learning dockerfiles
    remove machine-learning and machine-learning-ui as
    they are similar to clearlinux/tensorflow and
    stack/DLRS.
    
    Signed-off-by: Liu, Jianjun <jianjun.liu@intel.com>

--- machine-learning-ui/jupyter_notebook_config.py
+++
@@ -1,7 +0,0 @@
-c = get_config()
-c.ContentsManager.root_dir = "/root/"
-c.NotebookApp.allow_root = True
-c.NotebookApp.ip = '0.0.0.0'
-c.NotebookApp.open_browser = False
-c.NotebookApp.port = 8888
-
