01e54dc20904edd30cb3f48bf2f4af0340d3e640
Gong Sophia <sophia.gong@intel.com>
2019-11-06 10:09:48 +0800

    add node env
    Signed-off-by: Gong Sophia <sophia.gong@intel.com>

--- node/docker-entrypoint.sh
+++ node/docker-entrypoint.sh
@@ -5,4 +5,8 @@ if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
 	  set -- node "$@"
 fi
 
+echo "IMAGE NAME: $NAME"
+echo "IMAGE VERSION: $VERSION"
+echo "IMAGE PATH: $PATH"
+
 exec "$@"
