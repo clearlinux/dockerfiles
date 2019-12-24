00aeaebbcf26088984d68f55fba47a0a56be012e
Qi Zheng <qi.zheng@intel.com>
2019-12-24 15:48:44 +0800

    Update sample model to mobilenetv2-int8-tf-0001
    This model could show AVX512 VNNI performance advantage.
    
    Signed-off-by: Qi Zheng <qi.zheng@intel.com>

--- openvino/demo.sh
+++ openvino/demo.sh
@@ -2,6 +2,4 @@
 # Copyright (C) 2018 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-# demo to use alexnet modle to do classification
-
 classification_sample_async -i cat.bmp -m $MODEL_PATH/$MODEL_NAME.xml
