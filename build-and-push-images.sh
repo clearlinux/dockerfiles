e44821c40fc347a10f70983bb9379df85c7656f1
William Douglas <william.douglas@intel.com>
2024-06-17 09:32:04 -0700

    Rework release workflow to split up image creation
    Trying to run creation in a single job exhausts disk space while
    also being slower than running a matrix. As only two image are
    required for the others to build, run those in serial then the rest in
    a matrix.

--- build-and-push-images.sh
+++ build-and-push-images.sh
@@ -23,30 +23,4 @@ build_and_push_image() {
     push_image "${name}" "${ver}"
 }
 
-main() {
-    local ver="$1"
-    build_and_push_image os-core "${ver}"
-    # httpd is used by cgit so it must be pushed first
-    build_and_push_image httpd "${ver}"
-    build_and_push_image cgit "${ver}"
-    build_and_push_image golang "${ver}"
-    build_and_push_image haproxy "${ver}"
-    build_and_push_image iperf "${ver}"
-    build_and_push_image mariadb "${ver}"
-    build_and_push_image memcached "${ver}"
-    build_and_push_image nginx "${ver}"
-    build_and_push_image node "${ver}"
-    build_and_push_image numpy-mp "${ver}"
-    build_and_push_image perl "${ver}"
-    build_and_push_image php "${ver}"
-    build_and_push_image php-fpm "${ver}"
-    build_and_push_image postgres "${ver}"
-    build_and_push_image python "${ver}"
-    build_and_push_image rabbitmq "${ver}"
-    build_and_push_image r-base "${ver}"
-    build_and_push_image redis "${ver}"
-    build_and_push_image ruby "${ver}"
-    build_and_push_image tesseract-ocr "${ver}"
-}
-
-main $1
+build_and_push_image $1 $2
